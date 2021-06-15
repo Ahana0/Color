a8ef496c7596acf34345c9281a32c465697fd612
Tanjela Ahana <69899043+Ahana0@users.noreply.github.com>
2021-06-15 22:32:38 +0600

    2nd day works

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ app.set('views', path.join(__dirname,'views'));
 //end points
 app.get('/',(req,res)=>{
     const params = {}
-    res.status(200).render('index.pug',params);
+    res.status(200).render('home.pug',params);
+});
+app.get('/menu',(req,res)=>{
+    const params = {}
+    res.status(200).render('menu.pug',params);
+});
+app.get('/photos',(req,res)=>{
+    const params = {}
+    res.status(200).render('photos.pug',params);
+});
+app.get('/review',(req,res)=>{
+    const params = {}
+    res.status(200).render('review.pug',params);
+});
+app.get('/contact',(req,res)=>{
+    const params = {}
+    res.status(200).render('contact.pug',params);
 });
 //start the server 
 app.listen(port, ()=>{
